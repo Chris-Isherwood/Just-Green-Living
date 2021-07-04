58f2f45aa2cff3b0af9756a211d76e4b6ce4cb13
Chris Isherwood <chris.ish97@gmail.com>
2021-07-04 15:58:45 +0100

    Adding in connection to mongodb

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,36 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Just Green Living",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      // The name of the plugin
+      resolve: 'gatsby-source-mongodb',
+      options: {
+          // Name of the database and collection where are books reside
+          dbName: 'blog',
+          collection: 'posts',
+          server: {
+              address: 'just-green-living-shard-00-01.sjlvc.mongodb.net',
+              port: 27017
+          },
+          auth: {
+              user: process.env.MONGODB_USER,
+              password: process.env.MONGODB_PASSWORD
+          },
+          extraParams: {
+              replicaSet: 'Just-green-living-shard-0',
+              ssl: true,
+              authSource: 'admin',
+              retryWrites: true
+          }
+      }
+  },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 
-const IndexPage = () => {
+
+const IndexPage = (props) => {
+  const posts = props.data.allMongodbBlogPosts.edges;
+
   return (
-    <main>
-      <h1>Just Green Living</h1>
-    </main>
+    <div>
+      {posts.map(post =>
+      <div>
+        <h1>{post.node.Category}</h1>
+        <h4>{post.node.Author}</h4>
+        <p>{post.node.Content}</p>
+      </div>
+      )}
+    </div>
   )
 }
 
 export default IndexPage
+
+export const posts = graphql `
+query {
+  allMongodbBlogPosts {
+    edges {
+      node {
+        Author
+        Category
+        Content
+      }
+    }
+  }
+}
+`
